1ca4ee4d3e2f8b3b7023a7c0f36e26c1b6a172ce
AnweshDahal <anweshd2000@gmail.com>
2020-02-21 22:35:47 +0545

    styled and structured the code

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,20 @@
-function get(object){
-    console.log(object.value);
+var expression = document.getElementById("expression");
+
+var result = document.getElementById("result");
+
+function buttonClick(object){
+    // console.log("Helo")
+    if (object.value == "cls"){
+        expression.value = "";
+        result.value = 0;
+    } else if (object.value == "ans"){
+        try {
+            result.value = eval(expression.value);
+        } catch (error) {
+            result.value = "Syntax Error"
+        }
+    } else{
+        expression.value += object.value;
+    }
+    
 }
