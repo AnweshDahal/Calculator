f7a4f98d700256242feb9400d531f111ec709b0e
AnweshDahal <anweshd2000@gmail.com>
2020-02-22 10:42:45 +0545

    Added Keyboard Controls

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,19 @@
 var expression = document.getElementById("expression");
-
 var result = document.getElementById("result");
 
+window.onkeydown = function(e){
+    // this.alert(e.keyCode)
+        if(e.keyCode == 13){
+            document.getElementById("ans").click();
+        } else if(e.keyCode == 27){
+            this.document.getElementById("cls").click();
+        }
+    }
+
+function focusExp(){
+    expression.focus();
+}
 function buttonClick(object){
-    // console.log("Helo")
     if (object.value == "cls"){
         expression.value = "";
         result.value = 0;
@@ -15,6 +25,5 @@ function buttonClick(object){
         }
     } else{
         expression.value += object.value;
-    }
-    
+    }  
 }
